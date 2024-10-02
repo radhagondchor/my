040255982f3bb638a4452a72b6eb5573346ad2e1
Radha Gondchor <141768260+radhagondchor@users.noreply.github.com>
2024-10-02 12:48:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -200,7 +200,7 @@ resource "aws_instance" "public_instance_1" {
   ami                    = "ami-085f9c64a9b75eed5"
   instance_type          = "t2.medium"
   subnet_id              = aws_subnet.elastic_pub_subnet_1.id
-  key_name               = "elasticsearch"  # Update this line to use the existing key pair
+  key_name               = "tool"  # Update this line to use the existing key pair
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true  
 
@@ -213,7 +213,7 @@ resource "aws_instance" "public_instance_2" {
   ami                    = "ami-085f9c64a9b75eed5"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.elastic_pub_subnet_2.id
-  key_name               = "elasticsearch"  # Update this line to use the existing key pair
+  key_name               = "tool"  # Update this line to use the existing key pair
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
